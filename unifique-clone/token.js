c9f792a2081f14886896dfeebcc71fc8c680b3e4
amandafeller <amandaboehringer4@gmail.com>
2022-11-10 14:10:01 -0300

    ok

---
+++ unifique-clone/token.js
@@ -0,0 +1,8 @@
+function getUrlBase(port) {
+    const url = "https://gmrcmyaeuidororntsmf.supabase.co";
+    return url + "/rest/v1/" + port + "select=*";
+}
+
+function getTokenSupabase() {
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdtcmNteWFldWlkb3Jvcm50c21mIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMwMDEzNDksImV4cCI6MTk3ODU3NzM0OX0.cK2-Xx3GLz94j4x2Sc5B_Jf5XZ6C0XxhTHLzvAQPOHA";
+}
